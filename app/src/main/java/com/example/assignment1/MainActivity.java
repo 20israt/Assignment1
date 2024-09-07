8d58036130af47bd8ecb6f3a2e3d9c5014bb313f
Israt <israt5457@gmail.com>
2024-09-07 23:44:42 +0600

    First Commit

---
+++ app/src/main/java/com/example/assignment1/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.assignment1;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView textView = findViewById(R.id.text_view1);
+        ImageView imageView = findViewById(R.id.image);
+        TextView textView2 = findViewById(R.id.text_view2);
+        Button button = findViewById(R.id.btn_click);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Button Clicked!", Toast.LENGTH_SHORT).show();
+            }
+     });
+
+
+ }
+}
